5066aabf51169d671762376fe53ab4863c24801a
shin <>
2018-06-17 17:36:15 +0900

    namespace update

--- Utility/UI/CustomDetailsListView.cs
+++ Utility/UI/CustomDetailsListView.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace StockTrack.Util
+namespace Utility.UI
 {
     public class CustomDetailsListView : ListView
     {
