d63c5129aa5a88895db940266c9583323a7cf711
shin <>
2019-12-27 16:05:56 +0900

    window service, firewall 지원 클래스

---
+++ Utility/Window/WinFirewall.cs
@@ -0,0 +1,84 @@
+﻿using NetFwTypeLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Window
+{
+    /// <summary>
+    /// Reference dll: COM.NetFwTypeLib
+    /// </summary>
+    public class WinFirewall
+    {
+        public static bool AddRule(string ruleName, string applicationPath, string description = "")
+        {
+            INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+
+            if (isRuleExist(firewallPolicy, ruleName, applicationPath))
+                return true;
+
+            INetFwRule firewallRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+            firewallRule.Name = ruleName; // 방화벽 규칙을 구분하는 이름, 삭제시에도 사용됩니다
+            firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_ALLOW;
+            firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_IN;
+            firewallRule.ApplicationName = applicationPath;
+            firewallRule.InterfaceTypes = "All";
+            firewallRule.Description = description;
+            firewallRule.Enabled = true;
+
+            firewallPolicy.Rules.Add(firewallRule);
+
+            return isRuleExist(firewallPolicy, ruleName, applicationPath);
+        }
+
+        public static bool AddRule(string ruleName, int port, string description = "")
+        {
+            INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+
+            if (isRuleExist(firewallPolicy, ruleName, port))
+                return true;
+
+            INetFwRule firewallRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+            firewallRule.Name = ruleName; // 방화벽 규칙을 구분하는 이름, 삭제시에도 사용됩니다
+            firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_ALLOW;
+            firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_IN;
+            firewallRule.Protocol = (int)NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_TCP;
+            firewallRule.Description = description;
+            firewallRule.LocalPorts = port.ToString();
+            firewallRule.Enabled = true;
+
+            firewallPolicy.Rules.Add(firewallRule);
+
+            return isRuleExist(firewallPolicy, ruleName, port);
+        }
+
+        public static bool RemoveRule(string ruleName)
+        {
+            INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+
+            if (!isRuleExist(firewallPolicy, ruleName))
+                return true;
+
+            firewallPolicy.Rules.Remove(ruleName);
+
+            return !isRuleExist(firewallPolicy, ruleName);
+        }
+
+        private static bool isRuleExist(INetFwPolicy2 policy, string ruleName)
+        {
+            return policy.Rules.Cast<INetFwRule>().Any(rule => rule.Name == ruleName);
+        }
+
+        private static bool isRuleExist(INetFwPolicy2 policy, string ruleName, string applicationPath)
+        {
+            return policy.Rules.Cast<INetFwRule>().Any(rule => rule.Name == ruleName && rule.ApplicationName == applicationPath);
+        }
+
+        private static bool isRuleExist(INetFwPolicy2 policy, string ruleName, int port)
+        {
+            return policy.Rules.Cast<INetFwRule>().Any(rule => rule.Name == ruleName && rule.LocalPorts == port.ToString());
+        }
+    }
+}
---
+++ Utility/Window/WinService.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Window
+{
+    /// <summary>
+    /// Reference dll: System.ServiceProcess, System.Configuration.Install
+    /// </summary>
+    public class WinService
+    {
+        // 서비스 명령 대기 시간 
+        public static TimeSpan WAIT_TIME = TimeSpan.FromSeconds(20);
+
+        public static void ServiceListLog()
+        {
+            var services = ServiceController.GetServices();
+
+            foreach (var service in services)
+            {
+                Console.WriteLine(service.ServiceName + "[" + service.DisplayName + "] " + service.Status);
+            }
+        }
+
+
+        public static ServiceController GetService(string name)
+        {
+            var services = ServiceController.GetServices();
+            ServiceController service = services.FirstOrDefault(x => x.ServiceName == name);
+            return service;
+        }
+
+        public static bool IsRunning(ServiceController service)
+        {
+            if (service != null)
+            {
+                if (service.Status == ServiceControllerStatus.Running)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static void Start(ServiceController service)
+        {
+            if (service != null)
+            {
+                service.Start();
+                service.WaitForStatus(ServiceControllerStatus.Running, WAIT_TIME);
+            }
+        }
+
+        public static void Stop(ServiceController service)
+        {
+            if (service != null)
+            {
+                service.Stop();
+                service.WaitForStatus(ServiceControllerStatus.Stopped, WAIT_TIME);
+            }
+        }
+
+        /// <summary>
+        /// Service Install Command 지원 함수
+        /// </summary>
+        public static void Install(string path)
+        {
+            ManagedInstallerClass.InstallHelper(new string[] { path });
+        }
+
+        /// <summary>
+        /// Service Uninstall Command 지원 함수
+        /// </summary>
+        public static void Uninstall(string path)
+        {
+            ManagedInstallerClass.InstallHelper(new string[] { "/u", path });
+        }
+
+
+        public static void Install(ServiceController service, InstallEventHandler handler = null)
+        {
+            if (service != null)
+            {
+                ServiceInstaller installer = new ServiceInstaller();
+                installer.Context = new InstallContext();
+                installer.ServiceName = service.ServiceName;
+                installer.Install(null);
+
+                if (handler != null)
+                    installer.AfterInstall += handler;
+            }
+        }
+
+        public static void Uninstall(ServiceController service, InstallEventHandler handler = null)
+        {
+            if (service != null)
+            {
+                ServiceInstaller installer = new ServiceInstaller();
+                installer.Context = new InstallContext();
+                installer.ServiceName = service.ServiceName;
+                installer.Uninstall(null);
+
+                if (handler != null)
+                    installer.AfterUninstall += handler;
+            }
+        }
+    }
+}
