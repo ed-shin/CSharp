0b0606373134b3cdaed81bb2e6af465db3744315
DESKTOP-49K35E9\jpshin <jp.shin.kor@gmail.com>
2022-04-06 01:06:58 +0900

    Singleton 패턴 클래스 추가

---
+++ Utility/Pattern/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Pattern
+{
+    public abstract class Singleton<T> where T : class
+    {
+        private static readonly Lazy<T> _instance = new Lazy<T>(() => Activator.CreateInstance(typeof(T), true) as T);
+
+        public static T Instance 
+        { 
+            get { return _instance.Value; } 
+        }
+    }
+}
